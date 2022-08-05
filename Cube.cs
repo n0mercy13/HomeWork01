452567f0e5d88e459c07dfc73408e89646f9b1bf
n0mercy13 <89132411+n0mercy13@users.noreply.github.com>
2022-08-05 21:55:19 +0300

    testfile uploaded

---
+++ Cube.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstructClasses
+{
+    internal class Cube : Shape
+    {
+        public double Length { get; set; }
+        
+
+        public Cube(double length)
+        {
+            Name = "Cube";
+            Length = length;
+        }
+
+        public override double CalculateVolume()
+        {
+            return Math.Pow(Length,3);
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"The {Name} side has a length of {Length}");
+        }
+    }
+}
